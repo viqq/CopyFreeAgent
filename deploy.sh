cd4b3d8f2890381beba84d9c7a05e7b83b5d4e0a
antonPC <viqq@i.ua>
2015-07-11 01:17:04 +0300

    Fixes

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,8 @@ cd /Projects/FreeAgent/
 git pull
 mvn clean install -Dmaven.test.skip=true
 rm -r /var/lib/tomcat7/webapps/free-agent-base-1.0-SNAPSHOT/
-mv /Projects/FreeAgent/webbapp/target/free-agent-base-1.0-SNAPSHOT/ /var/lib/tomcat7/webapps/
+cd /Projects/FreeAgent/webbapp/target/
+mv ./free-agent-base-1.0-SNAPSHOT /var/lib/tomcat7/webapps/
 service tomcat7 restart
 
 echo "Finished"
