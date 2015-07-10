7e4d094c7d6960a57f73898b94feba789f5c9f5f
antonPC <viqq@i.ua>
2015-07-11 00:59:32 +0300

    Show exception

--- webapp/src/main/java/com/free/agent/controller/BaseController.java
+++ webapp/src/main/java/com/free/agent/controller/BaseController.java
@@ -1,16 +1,14 @@
 package com.free.agent.controller;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by antonPC on 23.06.15.
  */
-@ControllerAdvice
+//@ControllerAdvice
 public class BaseController {
 
-    @ExceptionHandler(Exception.class)
+    //@ExceptionHandler(Exception.class)
     public ModelAndView getFilms() {
         System.out.print("Exception!!!");
         ModelAndView model = new ModelAndView("login-form");
