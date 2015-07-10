e33004d770b412e20aa229c2ea7ca4825f23259b
antonPC <viqq@i.ua>
2015-07-11 00:57:49 +0300

    Fix ddl

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo "Deploying..."
 cd /Projects/FreeAgent/
 git pull
 mvn clean install -Dmaven.test.skip=true
-mv /Projects/FreeAgent/webbapp/target/free-agent-base-1.0-SNAPSHOT /var/lib/tomcat7/webapps/
+mv /Projects/FreeAgent/webbapp/target/free-agent-base-1.0-SNAPSHOT/ /var/lib/tomcat7/webapps/
 service tomcat7 restart
 
 echo "Finished"
