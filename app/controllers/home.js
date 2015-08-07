f70b1ef2ca45087f87ad4791e6cba05d8873e653
Tit Kovalenko <t.a.kvlnk@gmail.com>
2015-08-07 09:31:01 +0300

    fixed images, fix after bad merge

--- app/controllers/home.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by TITUS on 02.08.2015.
- */
-define(['app'], function (app) {
-    app.controller('HomeCtrl', function ($scope) {
-        $scope.message = "Message from LoginCTRL";
-    });
-});
