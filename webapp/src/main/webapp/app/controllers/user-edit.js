835e5c2dbc7549dccdb96be0cbc8b2c845554b16
Tit Kovalenko <t.a.kvlnk@gmail.com>
2015-09-28 21:52:42 +0300

    user data edit in process

---
+++ webapp/src/main/webapp/app/controllers/user-edit.js
@@ -0,0 +1,15 @@
+/**
+ * Created by TITUS on 27.09.2015.
+ */
+define(
+    [
+        'angularAMD',
+
+        'directives/header/script',
+        'directives/profile-edit/script',
+        'directives/footer/script'
+    ],
+    function (angularAMD) {
+        angularAMD.controller('UserEditCtrl', function() {});
+    }
+);
---
+++ webapp/src/main/webapp/app/services/edit-user-info.js
@@ -0,0 +1,25 @@
+/**
+ * Created by TITUS on 15.08.2015.
+ */
+define(['angularAMD'], function (angularAMD) {
+    var registration = function ($http) {
+        return function (data) {
+            return $http({
+                method: 'POST',
+                url: 'user',
+                data: data,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            })
+                .success(function(data) {
+                    console.log(data)
+                })
+                .error(function(err) {
+                    console.log(err);
+                });
+        }
+    };
+
+    angularAMD.factory('registration', ['$http', registration]);
+});
