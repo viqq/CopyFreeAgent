657fde918a66ec058c7030d89105c19cfe64c804
antonPC <viqq@i.ua>
2015-10-04 12:46:55 +0300

    fixes updateStatic script

--- updateStatic.sh
+++ updateStatic.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 #use only on prod
 
-rm -r /var/lib/tomcat7/webapps/free-agent-base-1.0-SNAPSHOT/css/
-rm -r /var/lib/tomcat7/webapps/free-agent-base-1.0-SNAPSHOT/js/
-rm -r /var/lib/tomcat7/webapps/free-agent-base-1.0-SNAPSHOT/fonts/
-rm -r /var/lib/tomcat7/webapps/free-agent-base-1.0-SNAPSHOT/WEB-INF/pages/
+rm -r /var/lib/tomcat7/webapps/ROOT/css/
+rm -r /var/lib/tomcat7/webapps/ROOT/js/
+rm -r /var/lib/tomcat7/webapps/ROOT/fonts/
+rm -r /var/lib/tomcat7/webapps/ROOT/WEB-INF/pages/
 
-cp -r /Projects/FreeAgent/webapp/src/main/webapp/css /var/lib/tomcat7/webapps/free-agent-base-1.0-SNAPSHOT/css/
-cp -r /Projects/FreeAgent/webapp/src/main/webapp/js /var/lib/tomcat7/webapps/free-agent-base-1.0-SNAPSHOT/js/
-cp -r /Projects/FreeAgent/webapp/src/main/webapp/fonts /var/lib/tomcat7/webapps/free-agent-base-1.0-SNAPSHOT/fonts/
-cp -r /Projects/FreeAgent/webapp/src/main/webapp/WEB-INF/pages/ /var/lib/tomcat7/webapps/free-agent-base-1.0-SNAPSHOT/WEB-INF/pages/
+cp -r /Projects/FreeAgent/webapp/src/main/webapp/css /var/lib/tomcat7/webapps/ROOT/css/
+cp -r /Projects/FreeAgent/webapp/src/main/webapp/js /var/lib/tomcat7/webapps/ROOT/js/
+cp -r /Projects/FreeAgent/webapp/src/main/webapp/fonts /var/lib/tomcat7/webapps/ROOT/fonts/
+cp -r /Projects/FreeAgent/webapp/src/main/webapp/WEB-INF/pages/ /var/lib/tomcat7/ROOT/WEB-INF/pages/
 
 echo "Static resourses were updated"
