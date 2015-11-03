e73fb72396df7eeabc281a74268baa0cbbe55f73
antonPC <viqq@i.ua>
2015-11-03 23:43:26 +0200

    fix link for prod

--- libs/service-free-agent/src/main/java/com/free/agent/service/util/LinkUtils.java
+++ libs/service-free-agent/src/main/java/com/free/agent/service/util/LinkUtils.java
@@ -4,7 +4,7 @@ package com.free.agent.service.util;
  * Created by antonPC on 03.11.15.
  */
 public final class LinkUtils {
-    private static final String HOST = "http://localhost:8080";
+    private static final String HOST = "http://78.47.247.157:8080";
 
     public static String getLinkForRegistration(String email, String randomString, boolean isPostponed) {
         String key = "key=" + EncryptionUtils.md5(email);
